5f8685e478e1c652b63f9b8187dd0bda56126682
Saran Tanpituckpong <sarunta@gmail.com>
2017-02-11 23:18:50 +0700

    Fulfill features

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 'use strict';
 
-module.exports = function (mainFunction, fallbackFunction) {
-	try {
-		return mainFunction();
-	} catch (err) {
-		return fallbackFunction();
+function evl(mainFunction, fallbackFunction, callNow = true) {
+	if (typeof mainFunction !== 'function') {
+		let mainValue = mainFunction;
+		mainFunction = () => mainValue;
 	}
-};
+	if (typeof fallbackFunction !== 'function') {
+		let fallbackValue = fallbackFunction;
+		fallbackFunction = () => fallbackValue;
+	}
+
+	if (callNow) {
+		try {
+			return mainFunction();
+		} catch (e) {
+			return fallbackFunction();
+		}
+	} else {
+		return function () {
+			if ((arguments.length == 2) && Array.isArray(arguments[0]) && Array.isArray(arguments[1])) {
+				try {
+					return mainFunction.apply(this, arguments[0]);
+				} catch (e) {
+					return fallbackFunction.apply(this, arguments[1]);
+				}
+			} else {
+				try {
+					return mainFunction.apply(this, arguments);
+				} catch (e) {
+					return fallbackFunction.apply(this, arguments);
+				}
+			}
+		};
+	}
+}
+
+module.exports = evl;
